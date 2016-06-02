965b0b37995ebe9443b42a453240cc44f6cca136
Patrick Engelkes <p.engelkes@deos-ag.com>
2016-06-02 15:24:10 +0200

    Add validation to users.

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,48 @@
 import DS from 'ember-data';
+import { validator, buildValidations } from 'ember-cp-validations';
 
-export default DS.Model.extend({
-  name: DS.attr('string'),
-  email: DS.attr('string'),
-  password: DS.attr('string'),
-  password_confirmation: DS.attr('string')
+var attr = DS.attr;
+
+const Validations = buildValidations({
+  name: [
+    validator('presence', {
+      presence: true,
+      message: "Benutzername darf nicht leer sein"
+    })
+  ],
+  email: [
+    validator('presence', {
+      presence: true,
+      message: "Email darf nicht leer sein"
+    }),
+    validator('format', {
+      type: 'mail',
+      message: "Entspricht nicht einer gültigen Email-Adresse"
+    })
+  ],
+  password: [
+    validator('presence', {
+      presence: true,
+      message: "Passwort darf nicht leer sein"
+    })
+  ],
+  password_confirmation: [
+    validator('presence', {
+      presence: true,
+      message: "Passwortbestätitung darf nicht leer sein"
+    }),
+    validator('confirmation', {
+      on: 'password',
+      message: 'Passwörter stimmen nicht überein.'
+    })
+  ]
+
+});
+
+
+export default DS.Model.extend(Validations, {
+  'name': attr('string'),
+  'email': attr('string'),
+  'password': attr('string'),
+  'password_confirmation': attr('string')
 });
--- app/register/route.js
+++ app/register/route.js
@@ -1,22 +1,25 @@
 import Ember from 'ember';
+import EmberValidations from 'ember-validations';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(EmberValidations, {
   flashMessages: Ember.inject.service(),
 
-  actions:
-  {
-    register()
-    {
+  model() {
+    return this.store.createRecord('user');
+  },
+
+  actions: {
+    register() {
       var user = this.store.createRecord('user', {
-        name : this.controller.get('name'),
-        email : this.controller.get('email'),
-        password : this.controller.get('password'),
-        password_confirmation : this.controller.get('password_confirmation')
+        name: this.controller.get('name'),
+        email: this.controller.get('email'),
+        password: this.controller.get('password'),
+        password_confirmation: this.controller.get('password_confirmation')
       });
       user.save().then(() => {
         this.transitionTo('index');
         this.get('flashMessages').success("Registration successful!");
-      }, function() {
+      }, function () {
         console.log("Registering new user failed");
       });
     }
